068645526436e24167b661f9b7cab7bfc6d7844d
Rashmi Sharma <rksharma2679@gmail.com>
2020-03-08 11:26:41 -0400

    chaning port from env variable

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 //sets up the express app to handle data parsing
